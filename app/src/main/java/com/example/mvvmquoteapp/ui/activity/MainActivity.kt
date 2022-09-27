2db295db5a5f1f119037cc46c0878ca52acd0535
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-09-28 01:21:01 +0200

    DataBinding Commit

--- app/src/main/java/com/example/mvvmquoteapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/mvvmquoteapp/ui/activity/MainActivity.kt
@@ -9,7 +9,7 @@ import com.example.mvvmquoteapp.databinding.ActivityMainBinding
 import com.example.mvvmquoteapp.ui.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
     private lateinit var quoteViewModel: QuoteViewModel
 
     @SuppressLint("SetTextI18n")
@@ -20,9 +20,11 @@ class MainActivity : AppCompatActivity() {
 
         val etTitle = binding.etTitle
         val etAuthor = binding.etAuthor
-        val tvQuote = binding.tvQuote
 
         quoteViewModel = ViewModelProvider(this)[QuoteViewModel::class.java]
+
+        binding.quoteModelView = this.quoteViewModel
+        binding.lifecycleOwner = this
 
         binding.button.setOnClickListener {
             if (etTitle.text.isBlank() || etAuthor.text.isBlank()) {
@@ -35,8 +37,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        quoteViewModel.data.observe(this) {
-            tvQuote.text = "${it.title.trim()} : ${it.author.trim()}"
-        }
     }
 }
